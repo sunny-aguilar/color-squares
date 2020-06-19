a620654f119cfb8c2bf848b029c0bfd03a2d3cc0
sunny-aguilar <sunny.aguilar@gmail.com>
2020-06-19 00:41:36 -0700

    color game

--- app.js
+++ app.js
@@ -59,35 +59,6 @@ function reset() {
     }
     h1.style.background = 'steelblue';
 }
-// hard and easy button event listeners
-// easyBtn.addEventListener('click', function() {
-//     hardBtn.classList.remove('selected');
-//     easyBtn.classList.add('selected');
-//     numSquares = 3;
-//     colors = generateRandomColors(numSquares);
-//     pickedColor = pickColor();
-//     colorDisplay.textContent = pickedColor;
-//     for (let i = 0; i < squares.length; i++) {
-//         if (colors[i]) {
-//             squares[i].style.background = colors[i];
-//         }
-//         else {
-//             squares[i].style.display = 'none';
-//         }
-//     }
-// }, false);
-// hardBtn.addEventListener('click', function() {
-//     easyBtn.classList.remove('selected');
-//     hardBtn.classList.add('selected');
-//     numSquares = 6;
-//     colors = generateRandomColors(numSquares );
-//     pickedColor = pickColor();
-//     colorDisplay.textContent = pickedColor;
-//     for (let i = 0; i < squares.length; i++) {
-//             squares[i].style.background = colors[i];
-//             squares[i].style.display = 'block';
-//     }
-// }, false);
 // add event listener to buton
 resetButton.addEventListener('click', reset, false);
 // update colorDisplay
