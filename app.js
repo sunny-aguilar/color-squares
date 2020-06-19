e78f3827347f567b74067cfefa3df477aca1b166
sunny-aguilar <sunny.aguilar@gmail.com>
2020-06-18 20:03:46 -0700

    color game

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+// commands
+// tsc app.js - compile code
+// tsc app.js --watch => compile as changes happen
+var colors = [
+    'rgb(255, 0, 0)',
+    'rgb(255, 255, 0)',
+    'rgb(0, 255, 0)',
+    'rgb(255, 0, 0)',
+    'rgb(0, 255, 255)',
+    'rgb(255, 0, 255)'
+];
