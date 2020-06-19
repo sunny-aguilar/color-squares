d27a93dc68b8a3d9da17af85f88fa60a2081b5c5
sunny-aguilar <sunny.aguilar@gmail.com>
2020-06-18 20:03:54 -0700

    color game

---
+++ app.ts
@@ -0,0 +1,21 @@
+// RGB color grid web app
+export {}
+
+// commands
+// tsc app.js - compile code
+// tsc app.js --watch => compile as changes happen
+
+
+
+let colors = [
+    'rgb(255, 0, 0)',
+    'rgb(255, 255, 0)',
+    'rgb(0, 255, 0)',
+    'rgb(255, 0, 0)',
+    'rgb(0, 255, 255)',
+    'rgb(255, 0, 255)'];
+
+
+
+
+
