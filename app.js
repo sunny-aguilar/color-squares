c442b16af07574f63a39320ab60172741d9c76e5
sunny-aguilar <sunny.aguilar@gmail.com>
2020-06-18 22:36:23 -0700

    color game

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ exports.__esModule = true;
 // commands
 // tsc app.js - compile code
 // tsc app.js --watch => compile as changes happen
-var colors = [
-    'rgb(255, 0, 0)',
-    'rgb(255, 255, 0)',
-    'rgb(0, 255, 0)',
-    'rgb(0, 255, 255)',
-    'rgb(0, 0, 255)',
-    'rgb(255, 0, 255)'
-];
+var colors = generateRandomColors();
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
--- app.ts
+++ app.ts
@@ -7,14 +7,7 @@ export {}
 
 
 
-let colors = [
-    'rgb(255, 0, 0)',
-    'rgb(255, 255, 0)',
-    'rgb(0, 255, 0)',
-    'rgb(0, 255, 255)',
-    'rgb(0, 0, 255)',
-    'rgb(255, 0, 255)'
-];
+let colors = generateRandomColors();
 
 let squares = document.querySelectorAll('.square');
 let pickedColor = pickColor();
