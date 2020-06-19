a32d12298afd5829e81bccc572abe072a3f41793
sunny-aguilar <sunny.aguilar@gmail.com>
2020-06-18 20:10:48 -0700

    color game

--- app.ts
+++ app.ts
@@ -11,8 +11,8 @@ let colors = [
     'rgb(255, 0, 0)',
     'rgb(255, 255, 0)',
     'rgb(0, 255, 0)',
-    'rgb(255, 0, 0)',
     'rgb(0, 255, 255)',
+    'rgb(0, 0, 255)',
     'rgb(255, 0, 255)'
 ];
 
@@ -20,7 +20,7 @@ let squares = document.querySelectorAll('.square');
 
 for(let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
-    squares[i].style.backgroundColor = colors[i];
+    // squares[i].style.backgroundColor = colors[i];
 }
 
 
